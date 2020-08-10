051577ba9541fa4865d5293a45851c50c9d4af69
Song Cho <deeplearner507@gmail.com>
2020-08-11 01:18:12 +0900

    array - twoSum

--- dataStructure/array/twoSum/solution.js
+++ dataStructure/array/twoSum/solution.js
@@ -2,34 +2,39 @@
 // @param {number} target
 // @return {number[]}
 
-var twoSum = function (nums, target) {
-  if (nums.length < 2) {
-    return false;
-  }
-  // let first;
-  let firstIndex = 0;
-  // let second;
-  nums.sort((a, b) => a - b);
-  console.log(nums)
-  let index = 1;
-  while (index < nums.length) {
-    console.log(firstIndex,index)
-    for (let i = index; i < nums.length; i++) {
-      if (nums[firstIndex] + nums[i] > target) {
-        return false;
-      } else if (nums[firstIndex] + nums[i] === target) {
-      
-        return [firstIndex, i];
+// Approach 1: Brute Force
+// The brute force approach is simple. Loop through each element xx and find if there is another value that equals to target - xtarget−x.
+
+// Complexity Analysis:
+// Time complexity : O(n^2). For each element, we try to find its complement by looping through the rest of array which takes O(n) time.
+// Therefore, the time complexity is O(n^2).
+
+// Space complexity : O(1).
+
+const twoSumType1 = function (nums, target) {
+  var ret = [];
+
+  for (var n = 0; n < nums.length; n++) {
+    for (var i = 1; i < nums.length; i++) {
+      if (nums[n] + nums[i] == target) {
+        ret.push([n, i]);
       }
     }
-
-    firstIndex++;
-    index++;
-    console.log(firstIndex,index)
   }
 
-  return true;
+  return ret;
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
+const twoSumType2 = function (nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
+};
+console.log(twoSumType2([2, 7, 11, 15], 9));
+console.log(twoSumType2([3, 5, 12, 2, 1], 4));
+console.log(twoSumType2([-3, 1, 2, 5, 0], 2));
+console.log(twoSumType2([3, 2, 4], 6));
