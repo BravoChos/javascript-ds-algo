c2529e86cce38defaeb8f0f3880cdb6080c76796
Song Cho <deeplearner507@gmail.com>
2020-08-13 23:02:20 +0900

    array - maximumSubarray problem statement

---
+++ dataStructure/array/maximumSubarray/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+
+
+
+
+
+  return true
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
