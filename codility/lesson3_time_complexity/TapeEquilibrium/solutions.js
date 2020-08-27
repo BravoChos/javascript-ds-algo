9765d55dac0aa68ca6e5e9410e99250c12fa8cd7
Song Cho <iamsongcho@gmail.com>
2020-08-27 23:02:06 +0900

    hashTable - firstRecurringCharacter done

--- codility/lesson3_time_complexity/TapeEquilibrium/solutions.js
+++ codility/lesson3_time_complexity/TapeEquilibrium/solutions.js
@@ -1,35 +1,28 @@
 function solution(A) {
-  // write your code in JavaScript (Node.js 8.9.4)
-  
-  let sum = A.reduce((a, b) => a + b, 0);
-  let diff = 0;
-  let index = 0;
-  
-  for(let i=0; i<A.length; i++){
-      let d = sum - A[i];
-      if(diff < d){
-          diff = d;
-          index = i;
+  let leftNum = 0;
+  let rightNum = A.reduce((a,b) => a+b);
+  let answer = null;
+  for (let i=0; i<A.length-1; i++) {
+      leftNum += A[i];
+      rightNum -= A[i];
+      const diff = Math.abs(leftNum - rightNum)
+      if (answer === null || answer > diff) {
+          answer = diff
       }
   }
-  
-  return index;
+  return answer
 }
 
 function solution2(A) {
-  // write your code in JavaScript (Node.js 8.9.4)
-  
-  let sum = A.reduce((a, b) => a + b, 0);
-  let diff = 0;
-  let index = 0;
-  
-  for(let i=0; i<A.length; i++){
-      let d = sum - A[i];
-      if(diff < d){
-          diff = d;
-          index = i;
-      }
+
+  let sumA = A.reduce((a, b) => a + b, 0)
+  let sumB = 0
+  let diff = []
+  for(let i=0; i<A.length-1; i++){
+      sumB += A[i]
+      sumA -= A[i]
+      diff[i] = Math.abs(sumA-sumB)
   }
-  
-  return index;
-}
+  D = diff.sort((a,b)=>a-b,0)
+  return D[0];
+  }
