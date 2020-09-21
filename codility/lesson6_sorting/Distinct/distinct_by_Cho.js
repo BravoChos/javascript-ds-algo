af59df0f9ab22653a0c25f72f25ce82772517045
Song Cho <iamsongcho@gmail.com>
2020-09-21 23:25:26 +0900

    codility - distinct problem solved

--- codility/lesson6_sorting/Distinct/distinct_by_Cho.js
+++ codility/lesson6_sorting/Distinct/distinct_by_Cho.js
@@ -1,6 +1,18 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
-
+//Detected time complexity:  O(N*log(N)) or O(N)
 function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
+  let set = {};
+  let distinct = 0;
+  for (let i = 0; i < A.length; i++) {
+    if (set[A[i]]) {
+    } else {
+      set[A[i]] = true;
+      distinct++;
+    }
+  }
+  return distinct;
 }
+
+console.log(solution([1, 2, 3, 2, 1, 3]));
--- codility/lesson6_sorting/Distinct/solutions.js
+++ codility/lesson6_sorting/Distinct/solutions.js
@@ -3,4 +3,11 @@
 
 function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
+  var map = {};
+  for (var i in A) {
+    if (!map[A[i]]) {
+      map[A[i]] = true;
+    }
+  }
+  return Object.keys(map).length;
 }
