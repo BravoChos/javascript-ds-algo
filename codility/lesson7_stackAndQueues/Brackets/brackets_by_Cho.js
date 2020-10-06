9dffa3bed39c13d4cda1b17d2548e2fb48fb52ec
Song Cho <iamsongcho@gmail.com>
2020-10-06 23:55:56 +0900

    codility - brackets problem solved

--- codility/lesson7_stackAndQueues/Brackets/brackets_by_Cho.js
+++ codility/lesson7_stackAndQueues/Brackets/brackets_by_Cho.js
@@ -3,4 +3,33 @@
 
 function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
+
+  if (A.length > 200000) {
+    return 0;
+  }
+  let openBracket = [];
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === "{" || A[i] === "[" || A[i] === "(") {
+      openBracket.push(A[i]);
+    }
+
+    if (A[i] === "}" && openBracket.pop() !== "{") {
+      return 0;
+    }
+    if (A[i] === "]" && openBracket.pop() !== "[") {
+      return 0;
+    }
+    if (A[i] === ")" && openBracket.pop() !== "(") {
+      return 0;
+    }
+  }
+
+  if (openBracket.length > 0) {
+    return 0;
+  }
+  return 1;
 }
+
+console.log(solution("{[()()]}")); //1
+console.log(solution("([)()]")); //0
+console.log(solution("{{{{"));
