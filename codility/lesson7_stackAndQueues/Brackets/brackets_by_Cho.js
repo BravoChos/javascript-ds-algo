7e25f36d2f4eda1cc0d1c5b214e53f2c83d3a14c
Song Cho <iamsongcho@gmail.com>
2020-10-07 23:52:19 +0900

    codility - fish problem description

--- codility/lesson7_stackAndQueues/Brackets/brackets_by_Cho.js
+++ codility/lesson7_stackAndQueues/Brackets/brackets_by_Cho.js
@@ -1,32 +1,22 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
+// Detected time complexity: O(N)
 function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
+  if (A.length > 200000) return 0;
 
-  if (A.length > 200000) {
-    return 0;
-  }
   let openBracket = [];
   for (let i = 0; i < A.length; i++) {
-    if (A[i] === "{" || A[i] === "[" || A[i] === "(") {
-      openBracket.push(A[i]);
-    }
+    if (A[i] === "{" || A[i] === "[" || A[i] === "(") openBracket.push(A[i]);
 
-    if (A[i] === "}" && openBracket.pop() !== "{") {
-      return 0;
-    }
-    if (A[i] === "]" && openBracket.pop() !== "[") {
-      return 0;
-    }
-    if (A[i] === ")" && openBracket.pop() !== "(") {
-      return 0;
-    }
-  }
+    if (A[i] === "}" && openBracket.pop() !== "{") return 0;
 
-  if (openBracket.length > 0) {
-    return 0;
+    if (A[i] === "]" && openBracket.pop() !== "[") return 0;
+
+    if (A[i] === ")" && openBracket.pop() !== "(") return 0;
   }
+  if (openBracket.length > 0) return 0;
   return 1;
 }
 
