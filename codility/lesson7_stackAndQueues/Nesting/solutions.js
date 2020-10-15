6281c75e1524c72467ff8c86f35432ed1c3c33fd
Song Cho <iamsongcho@gmail.com>
2020-10-15 22:41:12 +0900

    codility - nesting problem solved

--- codility/lesson7_stackAndQueues/Nesting/solutions.js
+++ codility/lesson7_stackAndQueues/Nesting/solutions.js
@@ -2,5 +2,21 @@
 // console.log('this is a debug message');
 
 function solution(S) {
-  // write your code in JavaScript (Node.js 8.9.4)
+  let counter = 0;
+
+  for (var i = 0; i < S.length; i++) {
+    switch (S[i]) {
+      case "(":
+        counter++;
+        break;
+
+      case ")":
+        counter--;
+        break;
+    }
+
+    if (counter < 0) return 0;
+  }
+
+  return counter === 0 ? 1 : 0;
 }
