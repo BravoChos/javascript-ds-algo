5efe0c47ed08f4e2bbfaed9cd2345a0524308754
Song Cho <iamsongcho@gmail.com>
2020-10-19 23:39:23 +0900

    codility - stoneWall problem fixed description

--- codility/lesson7_stackAndQueues/StoneWall/stoneWall_by_Cho.js
+++ codility/lesson7_stackAndQueues/StoneWall/stoneWall_by_Cho.js
@@ -1,20 +1,6 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
-// Detected time complexity: O(N)
-function solution(S) {
-  // write your code in JavaScript (Node.js 8.9.4)
-  let left = [];
-  for (let i = 0; i < S.length; i++) {
-    // console.log(S[i]);
-    if (S[i] === ")") {
-      if (left.length === 0) return 0;
-      left.pop();
-    }
-    if (S[i] === "(") left.push("(");
-  }
-  if (left.length > 0 || right.length > 0) return 0;
-  return 1;
-}
 
-console.log(solution("(()(())())")); // 1
-console.log(solution("())")); // 0
+function solution(H) {
+  // write your code in JavaScript (Node.js 8.9.4)
+}
