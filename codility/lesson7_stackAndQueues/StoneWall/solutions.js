7c8d08a6a24df276b18ecbd3345ac5542d09f7aa
Song Cho <iamsongcho@gmail.com>
2020-10-20 23:15:53 +0900

    codility - stoneWall problem solved

--- codility/lesson7_stackAndQueues/StoneWall/solutions.js
+++ codility/lesson7_stackAndQueues/StoneWall/solutions.js
@@ -3,4 +3,29 @@
 
 function solution(H) {
   // write your code in JavaScript (Node.js 8.9.4)
+  var stack = [];
+  var head = -1;
+  var block = 0;
+  var i = 0;
+  while (i < H.length) {
+    var h = H[i];
+    if (head < 0) {
+      ++head;
+      stack[head] = h; //stack push
+      ++i;
+    } else if (stack[head] == h) {
+      ++i;
+    } else if (stack[head] < h) {
+      ++head;
+      stack[head] = h; //stack push
+      ++i;
+    } else {
+      //stack[head] > h
+      --head; //stack pop
+      ++block;
+    }
+  }
+  return block + head + 1;
 }
+
+console.log(solution([8, 8, 5, 7, 9, 8, 7, 4, 8]));
