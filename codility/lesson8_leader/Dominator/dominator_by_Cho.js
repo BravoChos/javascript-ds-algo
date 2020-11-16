cdec1c014499f0870cb2d22ca157ad040d7e559c
Song Cho <iamsongcho@gmail.com>
2020-11-16 23:50:47 +0900

    codility - dominator problem solved

--- codility/lesson8_leader/Dominator/dominator_by_Cho.js
+++ codility/lesson8_leader/Dominator/dominator_by_Cho.js
@@ -3,4 +3,34 @@
 
 function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
+
+  let dominator = {};
+  for (let i = 0; i < A.length; i++) {
+    if (dominator[A[i]]) {
+      dominator[A[i]].count++;
+      dominator[A[i]].result.push(i);
+    } else {
+      dominator[A[i]] = {
+        count: 1,
+        result: [i],
+      };
+    }
+  }
+
+  let result = [];
+  let countMaxNumberOfElement = 0;
+  let twoDominators = false;
+  // not supported in Node 8.X version
+  Object.keys(dominator).forEach((item) => {
+    if (dominator[item].count > countMaxNumberOfElement) {
+      countMaxNumberOfElement = dominator[item].count;
+      result = dominator[item].result;
+      twoDominators = false;
+    } else if (dominator[item].count === countMaxNumberOfElement) {
+      twoDominators = true;
+    }
+  });
+  if (twoDominators) return -1;
+  return result;
 }
+console.log(solution([3, 4, 3, 2, -1, -1, 2])); // [0,2,4,6, 7]
--- codility/lesson8_leader/Dominator/solutions.js
+++ codility/lesson8_leader/Dominator/solutions.js
@@ -1,6 +1,15 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
+//Detected time complexity: O(N*log(N)) or O(N)
 function solution(A) {
-  // write your code in JavaScript (Node.js 8.9.4)
+  const arr = [];
+  for (let i = 0; i < A.length; i++) {
+    if (!arr[A[i]]) arr[A[i]] = 1;
+    else arr[A[i]]++;
+    if (arr[A[i]] > A.length / 2) return i;
+  }
+  return -1;
 }
+
+console.log(solution([3, 4, 3, 2, -1, 3, 3])); // [0,2,4,6,7]
