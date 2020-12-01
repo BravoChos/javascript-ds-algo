22336a324ca32c4de3dc74471ca4780ed6cb8eb3
Song Cho <iamsongcho@gmail.com>
2020-12-01 23:50:22 +0900

    codility - countFactors problem solved

--- codility/lesson10_prime_and_composite_numbers/CountFactors/countFactors_by_Cho.js
+++ codility/lesson10_prime_and_composite_numbers/CountFactors/countFactors_by_Cho.js
@@ -1,6 +1,20 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
+// Detected time complexity: O(sqrt(N))
 function solution(N) {
   // write your code in JavaScript (Node.js 8.9.4)
+
+  let i = 1;
+  let result = 0;
+
+  while (i * i < N) {
+    if (N % i === 0) {
+      result += 2;
+    }
+    i += 1;
+  }
+  if (i * i === N) result += 1;
+
+  return result;
 }
