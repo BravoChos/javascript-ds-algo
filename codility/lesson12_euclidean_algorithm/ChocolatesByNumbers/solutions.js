efc22130c3d3b2ef3f0f9773457a25a5754c9977
Song Cho <iamsongcho@gmail.com>
2020-12-25 23:54:27 +0900

    codility - chocolates by numbers add one solution

--- codility/lesson12_euclidean_algorithm/ChocolatesByNumbers/solutions.js
+++ codility/lesson12_euclidean_algorithm/ChocolatesByNumbers/solutions.js
@@ -3,4 +3,24 @@
 
 function solution(N, M) {
   // write your code in JavaScript (Node.js 8.9.4)
+  if(N === 1) return 1;
+
+  let arr = [];
+  for(let i = 0; i < N; i++) {
+      arr.push(true);
+  }
+
+  let cnt = 0;
+
+  for(let i = 0; i < N; i++) {
+      let idx = (i * M) % N
+      if(arr[idx] === true) {
+          arr[idx] = false;
+          cnt++;
+      } else {
+          break;
+      }
+  }
+
+  return cnt;
 }
