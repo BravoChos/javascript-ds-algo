5a32d8427d491e13aa8cfa722c61897804b23fd5
Song Cho <iamsongcho@gmail.com>
2021-01-27 23:57:15 +0900

    topInterviewQ - add valid_palinedrom solution one

---
+++ topInterviewQuestions/06_valid_palinedrome/solution.js
@@ -0,0 +1,16 @@
+const string = "A man, a plan, a canal: Panama"
+
+// reverse solution
+const isValidPalindrome = function(s) {
+    s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let rev = "";
+    
+    // generate a reverse string using a reverse for loop.
+    for(let i = s.length - 1; i >= 0; i--) {
+        rev += s[i];
+    }
+
+    return rev === s;
+};
+
+console.log(isValidPalindrome(string));
