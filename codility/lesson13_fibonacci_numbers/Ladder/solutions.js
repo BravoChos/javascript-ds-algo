87dc4a618a2e86dc49691babe597438f1fba82e2
Song Cho <iamsongcho@gmail.com>
2021-02-04 23:55:05 +0900

    codility - ladder add one solution

--- codility/lesson13_fibonacci_numbers/Ladder/solutions.js
+++ codility/lesson13_fibonacci_numbers/Ladder/solutions.js
@@ -2,5 +2,24 @@
 // console.log('this is a debug message');
 
 function solution(A, B) {
-    // write your code in JavaScript (Node.js 8.9.4)
+  // write your code in JavaScript (Node.js 8.9.4)
+
+  const L = A.length;
+  const max = Math.pow(2, 30);
+
+  const fibonacci = [0, 1, 2];
+
+  for (let i = 3; i < L + 1; i++) {
+    // L까지 피보나치 수열(방법의 수) 구해놓기
+    fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % max;
+  }
+
+  const result = [];
+
+  for (let i = 0; i < L; i++) {
+    // 2^B[i]의 수로 나누기
+    result[i] = fibonacci[A[i]] % Math.pow(2, B[i]);
+  }
+
+  return result;
 }
