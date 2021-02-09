e74c7d735861dd7a55478591ea01221ba728710e
Song Cho <iamsongcho@gmail.com>
2021-02-09 23:22:17 +0900

    codility - add one min max division problem solution

--- codility/lesson14_binary_search_algorithm/MinMaxDivision/solutions.js
+++ codility/lesson14_binary_search_algorithm/MinMaxDivision/solutions.js
@@ -2,5 +2,34 @@
 // console.log('this is a debug message');
 
 function solution(K, M, A) {
-    // write your code in JavaScript (Node.js 8.9.4)
+    var begin = A.reduce((a, v) => (a + v), 0)
+    begin = parseInt((begin+K-1)/K, 10);
+    var maxA = Math.max(A);
+    if (maxA > begin) begin = maxA;
+
+    var end = begin + M + 1;
+    var res = 0;
+
+    while(begin <= end) {
+        var mid = (begin + end) / 2;
+        var sum = 0;
+        var block = 1;
+        for (var ind in A) {
+            var a = A[ind];
+            sum += a;
+            if (sum > mid) {
+                ++block;
+                if (block > K) break;
+                sum = a;
+            }
+        }
+        if (block > K) {
+            begin = mid + 1;
+        } else {
+            res = mid;
+            end = mid - 1;
+        }
+    }
+    return res;
 }
+
