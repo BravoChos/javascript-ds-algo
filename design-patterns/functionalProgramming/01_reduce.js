02f76b1ee98abf61337410e4a7b67e75cd612474
BravoChos <bravochos@gmail.com>
2023-02-12 04:59:52 +0900

    fp - 01 reduce - example 2

--- design-patterns/functionalProgramming/01_reduce.js
+++ design-patterns/functionalProgramming/01_reduce.js
@@ -3,6 +3,7 @@
 
 const arr = [1, 2, 3, 4, 5];
 
+// #1.
 arr.reduce((x, y) => {
   console.log(`${x}+${y} = ${x + y}`);
   return x + y;
@@ -12,3 +13,11 @@ const sum = (x, y) => x + y;
 const result = arr.reduce(sum);
 
 console.log(result);
+
+// #2.
+const average = (sum, val, i, arr) => {
+  sum += val;
+  return i === arr.length - 1 ? sum / arr.length : sum;
+};
+
+console.log(arr.reduce(average));
