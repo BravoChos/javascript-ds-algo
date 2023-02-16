e7c9947dfd1e746dd642290ac7d9398fe3b9d3d7
BravoChos <bravochos@gmail.com>
2023-02-16 23:42:30 +0900

    fp - 02 map - example 2

--- design-patterns/functionalProgramming/02_map.js
+++ design-patterns/functionalProgramming/02_map.js
@@ -9,3 +9,23 @@ const mapped = arr.map((value) => {
 });
 
 console.log(mapped);
+
+// #02.
+const location = [
+  { city: "seoul", temp: 86 },
+  { city: "london", temp: 85 },
+  { city: "toronto", temp: 72 },
+];
+
+// 5/9 * (f - 32)
+
+const toCelcius = (obj) => {
+  return {
+    ...obj,
+    temp: Math.floor((5 / 9) * (obj.temp - 32)),
+  };
+};
+
+const result = location.map(toCelcius);
+
+console.log(result);
