7ced0e9971e9e6d28fcf7b7b9b34cd767e289abb
BravoChos <bravochos@gmail.com>
2023-02-20 18:29:18 +0900

    fp - 03 filter - example 3

--- design-patterns/functionalProgramming/03_filter.js
+++ design-patterns/functionalProgramming/03_filter.js
@@ -16,3 +16,15 @@ const values = [1, "", 2, undefined, 3, 4];
 const filtered = values.filter((value) => Boolean(value));
 
 console.log(filtered);
+
+// #03.
+const users = [
+  { name: "John", age: 25, gender: "male" },
+  { name: "Cho", age: 31, gender: "female" },
+  { name: "Kim", age: 20, gender: "female" },
+  { name: "Chase", age: 40, gender: "male" },
+];
+
+const olderThen30 = (user) => user.age > 30;
+
+console.log(users.filter(olderThen30));
