0ce980fe86c46fcdaa183afb29aae5e4b1c70821
BravoChos <bravochos@gmail.com>
2023-03-07 00:10:00 +0900

    dp - 03 observer - intro

---
+++ design-patterns/03_observer.js
@@ -0,0 +1,4 @@
+// Observer Pattern
+// a design pattern frequently used in object-oriented programming.
+// reduces dependencies between objects while allowing for easy handling of notifications
+// about changes in an object's state.
