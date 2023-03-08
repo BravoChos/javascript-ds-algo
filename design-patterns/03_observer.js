addbe8c07288026383ba4dfa24af7160c82ae2e6
BravoChos <bravochos@gmail.com>
2023-03-08 09:14:30 +0900

    dp - 03 observer - example 1

--- design-patterns/03_observer.js
+++ design-patterns/03_observer.js
@@ -2,3 +2,35 @@
 // a design pattern frequently used in object-oriented programming.
 // reduces dependencies between objects while allowing for easy handling of notifications
 // about changes in an object's state.
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+  getObserverList() {
+    return this.observers;
+  }
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+  unsubscribe(observer) {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+  notifyAll() {
+    this.observers.forEach((subscriber) => {
+      try {
+        subscriber.update(this.constructor.name);
+      } catch (err) {
+        console.log("err is", err);
+      }
+    });
+  }
+}
+
+class Observer {
+  constructor(name) {
+    this.name = name;
+  }
+  update(subj) {
+    console.log(`${this.name}: notified from ${subj} class!`);
+  }
+}
