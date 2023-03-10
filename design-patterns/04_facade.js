0b5a4bf307a5a5d24f77d89839b59fe7bd9e64d9
BravoChos <bravochos@gmail.com>
2023-03-11 00:00:01 +0900

    dp - 04 facade intro

---
+++ design-patterns/04_facade.js
@@ -0,0 +1,35 @@
+// Facade Pattern
+
+// It is a software design pattern that provides a simplified interface
+// to a complex system of classes, interfaces, and objects.
+// It acts as a "facade" or simplified entry point to a larger body of code, making it easier to use and understand.
+
+class Kitchen {
+  cookBurger() {
+    console.log("cooking burger");
+  }
+  cookSide() {
+    console.log("cooking side dishes");
+  }
+  prepareDrinks() {
+    console.log("preparing drinks");
+  }
+}
+
+class FoodService {
+  serve() {
+    console.log("Order ready, serving food now");
+  }
+}
+
+class RestaurantFacade {
+  newOrder() {
+    const kitchen = new Kitchen();
+    kitchen.cookBurger();
+    kitchen.cookSide();
+    kitchen.prepareDrinks();
+
+    const FoodService = new FoodService();
+    return FoodService.serve();
+  }
+}
