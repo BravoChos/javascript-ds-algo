1c9478eee47f51b2a5185ff99db086c74d3adc33
BravoChos <bravochos@gmail.com>
2023-03-13 18:04:22 +0900

    dp - 05 command - intro

---
+++ design-patterns/05_command.ts
@@ -0,0 +1,5 @@
+// Command Pattern
+
+// It is a behavioral design pattern that encapsulates a request as an object,
+// thereby allowing for the parameterization of clients with different requests,
+// queues or log requests, and supports undoable operations.
