e4a766fa74e2eb061d69813e98edfab5b1eb9889
BravoChos <bravochos@gmail.com>
2023-03-14 14:42:52 +0900

    dp - 05 command - example 2: interface

--- design-patterns/05_command.ts
+++ design-patterns/05_command.ts
@@ -3,3 +3,8 @@
 // It is a behavioral design pattern that encapsulates a request as an object,
 // thereby allowing for the parameterization of clients with different requests,
 // queues or log requests, and supports undoable operations.
+
+// Interface
+abstract class ICommand {
+    abstract execute(args): void;
+}
