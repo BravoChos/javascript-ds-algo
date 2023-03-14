16a2d68bdc76259c4f585893135779541b7768d6
BravoChos <bravochos@gmail.com>
2023-03-15 01:09:03 +0900

    dp - 05 command - example 2: reciever

--- design-patterns/05_command.ts
+++ design-patterns/05_command.ts
@@ -8,3 +8,19 @@
 abstract class ICommand {
     abstract execute(args): void;
 }
+
+// Reciever
+class State {
+    private _state: number;
+    constructor(state) {
+        this._state = state;
+    }
+
+    getState() {
+        return this._state;
+    }
+
+    setState(value) {
+        this._state = value;
+    }
+}
