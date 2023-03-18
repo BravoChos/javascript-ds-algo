b759cee1d6c86c331c18c93e1aeb985a0c7a4ce8
BravoChos <bravochos@gmail.com>
2023-03-19 00:00:10 +0900

    dp - 06 proxy - intro

---
+++ design-patterns/06_proxy.js
@@ -0,0 +1,7 @@
+// Proxy Pattern
+
+// IT is a structural design pattern that provides a surrogate or placeholder
+// for another object to control access to it.
+
+// In other words, the Proxy acts as an intermediary between the client and the real object,
+// allowing the client to interact with the proxy instead of the real object.
