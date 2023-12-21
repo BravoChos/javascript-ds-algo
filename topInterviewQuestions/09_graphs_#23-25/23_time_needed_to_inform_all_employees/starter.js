dd3f0037064bd3f15fede88c5782e3825dac3c37
BravoChos <bravochos@gmail.com>
2023-12-22 04:35:49 +0900

    topInterviewQ - add time needed to inform all employees

---
+++ topInterviewQuestions/09_graphs_#23-25/23_time_needed_to_inform_all_employees/starter.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @param {number} headID
+ * @param {number[]} manager
+ * @param {number[]} informTime
+ * @return {number}
+ */
+var numOfMinutes = function (n, headID, manager, informTime) {};
